3f235cd482730c0db11e92e85772a1e8aafb2fcf
jonathan louie <jonathan.louie@hotmail.com>
2023-07-03 11:28:31 -0700

    twosums

--- codingpractice/src/main/java/com/louie/codingpractice/LeetCode/LeetCode.java
+++ codingpractice/src/main/java/com/louie/codingpractice/LeetCode/LeetCode.java
@@ -1,5 +1,14 @@
 package com.louie.codingpractice.LeetCode;
 
 public class LeetCode {
-    
+    public int[] twoSum(int[] nums, int target) {
+        // todo guard clause for array[1]
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+        return new int[] { 0, 0 };
+    }
 }
--- codingpractice/src/test/java/com/louie/codingpractice/LeetCode/LeetCodeTest.java
+++ codingpractice/src/test/java/com/louie/codingpractice/LeetCode/LeetCodeTest.java
@@ -7,8 +7,22 @@ public class LeetCodeTest {
     /**
      * Rigorous Test :-)
      */
+
+    LeetCode Lc = new LeetCode();
+
     @Test
     public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void twoSumTest(){
+        int[] nums = {0,2,4,5};
+        int target = 6;
+        int[] result = Lc.twoSum(nums, target);
+        int[] expected = new int[]{1,2};
+        boolean isTrue = result.equals(new int[]{1,2});
+        System.out.println(isTrue);
         assertTrue(true);
     }
 }
